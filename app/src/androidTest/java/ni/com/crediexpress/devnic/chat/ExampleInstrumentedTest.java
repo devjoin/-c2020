c013b74609898915180669932ef81d448d2dee56
mmojica006 <musicmojica@gmail.com>
2018-04-18 09:47:01 -0600

    Adding layout

--- app/src/androidTest/java/ni/com/crediexpress/devnic/chat/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ni/com/crediexpress/devnic/chat/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.crediexpress.chat;
+package ni.com.crediexpress.devnic.chat;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.crediexpress.chat", appContext.getPackageName());
+        assertEquals("ni.com.crediexpress.devnic.chat", appContext.getPackageName());
     }
 }
